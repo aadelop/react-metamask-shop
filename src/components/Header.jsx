44061e0606d16a9c24ad2cdf59343886a87b8e9f
Alfredo Delgado L <aadelop@gmail.com>
2024-07-24 12:53:40 +0200

    Added navbar bootstrap element to Header component@

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,23 @@
+import { Link } from "react-router-dom";
+
 export function Header(){
-    return <div><h1>Header component</h1></div>
+    return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/products">Products</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/balance">Balance ETH</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      )
 }
