7701399a1b850c9468769ed62b384ee45c4e073e
Alfredo Delgado L <aadelop@gmail.com>
2024-07-24 12:17:38 +0200

    Added Router librarie of React, and defining initial Route

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Route, Routes  } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Shop aplication with React and Metamask</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<h1> Root path 1 </h1>}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>,
 )
