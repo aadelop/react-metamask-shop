367d5052d9f0cdfeaf87f94da3c3c3b8802c48cd
Alfredo Delgado L <aadelop@gmail.com>
2024-07-24 12:28:25 +0200

    Added initial basic component of the application

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes  } from 'react-router-dom'
+import {Home} from './components/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element= {<h1> Root path 1 </h1>}>
+        <Route path="/" element= {<Home></Home>}>
         </Route>
       </Routes>
     </BrowserRouter>
