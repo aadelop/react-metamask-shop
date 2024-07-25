fda4d9081f364704c1929caaa802e8b583ef9e60
Alfredo Delgado L <aadelop@gmail.com>
2024-07-25 10:32:08 +0200

    Added react-query to main.sjx and adapted QueryClientProvider to our app

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes  } from 'react-router-dom'
 import {Home} from './components/Home'
+import { Product } from './components/Product'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <QueryClientProvider client={queryClient}></QueryClientProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element= {<Home></Home>}>
-          <Route path="/products" element={<span> Products </span>}></Route>
+          <Route path="/products" element={<Product></Product>}></Route>
           <Route path="/balance" element={<span> Balance </span>}></Route>
         </Route>
       </Routes>
