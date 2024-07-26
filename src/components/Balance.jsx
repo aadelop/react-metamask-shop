10d025c33abdada92f8ec210b235c8559207bf45
Alfredo Delgado L <aadelop@gmail.com>
2024-07-26 11:45:14 +0200

    Added logic to connect with metamask and get balance of diferent accounts

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,4 +1,22 @@
+import {useState, useEffect } from 'react'
+const {ethereum} = window
+
 export function Balance() {
-    return <h2>Balance</h2>
+    const [cuenta, setCuenta] = useState(null)
+    useEffect(() => {
+        ethereum && ethereum.request({method: 'eth_requestAccounts'}).then(cuenta => {
+            setCuenta(cuenta[0])
+        ethereum.on('accountsChanged', (i)=>{
+                setCuenta(i[0])
+            })
+        })
+    }, [])
+    
+    
+    if(!ethereum){
+        return <div>No metamask connection detected</div>
+    }
+    
+    return <h2> { cuenta ? cuenta : "Cargando..."} </h2>
     
 }
