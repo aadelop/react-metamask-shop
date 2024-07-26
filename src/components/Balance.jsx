21af58895df4d8c1600bc8a23af425679f2cfd9e
Alfredo Delgado L <aadelop@gmail.com>
2024-07-26 13:07:03 +0200

    Included a second useEffect to allow use account and balance in the balance page

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,8 +1,12 @@
 import {useState, useEffect } from 'react'
+import {ethers} from 'ethers'
+
 const {ethereum} = window
+
 
 export function Balance() {
     const [cuenta, setCuenta] = useState(null)
+    const [balance, setBalance] = useState(null)
     useEffect(() => {
         ethereum && ethereum.request({method: 'eth_requestAccounts'}).then(cuenta => {
             setCuenta(cuenta[0])
@@ -11,12 +15,28 @@ export function Balance() {
             })
         })
     }, [])
+
+    useEffect(() => {
+        if(cuenta){
+            console.log("writing ethers",ethers);
+            const provider = new ethers.BrowserProvider(ethereum);
+            provider.getBalance(cuenta).then(balance =>{
+                console.log(ethers.formatEther(balance) );
+                setBalance(ethers.formatEther(balance) );
+            })
+        }
+        
+    }, [cuenta])
     
     
     if(!ethereum){
         return <div>No metamask connection detected</div>
     }
     
-    return <h2> { cuenta ? cuenta : "Cargando..."} </h2>
+    return (<div> 
+            <p> { cuenta ? cuenta : "Cargando..."}</p>
+            <p> { balance ? balance : "Cargando..."}</p>
+            </div>
+    )   
     
 }
