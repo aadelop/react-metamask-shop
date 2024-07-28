483fb8ba6719e27f4b4f539af49613f6ced64218
Alfredo Delgado L <aadelop@gmail.com>
2024-07-28 09:32:53 +0200

    addedreact hook form and logic to send a transaction in the sepolia test net

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,12 +1,17 @@
 import {useState, useEffect } from 'react'
 import {ethers} from 'ethers'
+import { useForm } from 'react-hook-form'
 
 const {ethereum} = window
 
 
 export function Balance() {
+    const {register, handleSubmit} = useForm();
     const [cuenta, setCuenta] = useState(null)
     const [balance, setBalance] = useState(null)
+    const [ok, setOk] = useState(null)
+    const [ko, setKo] = useState(null)
+
     useEffect(() => {
         ethereum && ethereum.request({method: 'eth_requestAccounts'}).then(cuenta => {
             setCuenta(cuenta[0])
@@ -27,6 +32,25 @@ export function Balance() {
         }
         
     }, [cuenta])
+
+    async function  submit(data){
+       const parameters = {
+        from:cuenta,
+        to: data.address,
+        value: ethers.parseEther(data.amount).toString(16)
+       }
+       console.log("Priting parameters", parameters);
+
+       try {
+            const txHash = await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [parameters]
+            })
+            setOk(txHash)
+       } catch (error) {
+            setKo(error)
+       }
+    }
     
     
     if(!ethereum){
@@ -36,6 +60,19 @@ export function Balance() {
     return (<div> 
             <p> { cuenta ? cuenta : "Cargando..."}</p>
             <p> { balance ? balance : "Cargando..."}</p>
+            <form className="form-inline" onSubmit={handleSubmit(submit)}>
+                <div className='form-group mb-3'>
+                    <label htmlFor="address">Address</label>
+                    <input id="address" defaultValue="0xd1a234c143e9668ebb053c99535e8e7744ee1152" className="form-control" {...register("address")} /> 
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="amount">Amount</label>
+                    <input id="amount" defaultValue="0.0042" className="form-control" {...register("amount")} /> 
+                </div>
+                <button type="submit" className="btn btn-primary mb-3" >Submit</button>
+            </form>
+            {ok && <div>{ok}</div>}
+            {ko && <div>{ko}</div>}
             </div>
     )   
     
